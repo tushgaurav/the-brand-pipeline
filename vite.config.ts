68daf6235a292a0a07a1a151082c981b8081df89
Tushar Gaurav <tushar.gaurav416@gmail.com>
2026-03-03 22:06:51 +0530

    chore(vite.config): remove react-router plugin from Vite configuration to streamline build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), reactRouter()],
+  plugins: [react(), tailwindcss()],
 })
